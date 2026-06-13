74284aa2bf238f048330abb8877749764cd7f9cd
Balkishan <balkishan.m@beyoung.in>
2026-06-13 16:03:46 +0530

    changes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,13 +12,31 @@ export default function Navbar({getMovies}) {
   };
   return (
     <div className="navbar">
-      <Link to="/" className="logo">CineVision</Link>
+      <Link to="cinevision/" className="logo">
+        CineVision
+      </Link>
       <div className="search">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="search-icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="search-icon"
+        >
           <circle cx="11" cy="11" r="8"></circle>
           <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
         </svg>
-        <input ref={inputQuery} onChange={handleSearch} type="text" placeholder="Search movies, actors..." />
+        <input
+          ref={inputQuery}
+          onChange={handleSearch}
+          type="text"
+          placeholder="Search movies, actors..."
+        />
       </div>
     </div>
   );
