6acae8e96e17967e2d94b09d4dbd2a789239f215
Balkishan <balkishan.m@beyoung.in>
2026-06-13 15:46:03 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,28 @@ import Navbar from "./components/Navbar";
 import Newmovie from "./components/Newmovie";
 
 function App() {
-     const [movies, setMovies] = useState([]);
-     const location = useLocation();
+  const [movies, setMovies] = useState([]);
+  const location = useLocation();
 
-     const getMovies = async (query) => {
-       const res = await fetch(
-         `https://www.omdbapi.com/?apikey=3744b0f7&s=${query}`,
-       );
-       const data = await res.json();
-       console.log(data);
-       setMovies(data.Search || []);
-     };
+  const getMovies = async (query) => {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=3744b0f7&s=${query}`,
+    );
+    const data = await res.json();
+    //  console.log(data);
+    setMovies(data.Search || []);
+  };
 
-     useEffect(() => {
-       getMovies("Dune");
-     }, []);
+  useEffect(() => {
+    getMovies("Dune");
+  }, []);
+
   return (
     <>
       <Navbar getMovies={getMovies} />
-      {location.pathname === '/' && <Newmovie />}
+      {location.pathname === "cinevision/" && <Newmovie />}
       <Routes>
-        <Route path="/" element={<Home movies={movies} />} />
+        <Route path="cinevision/" element={<Home movies={movies} />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </>
